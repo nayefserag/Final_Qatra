a60e24ac58ae1f5c7f50fadba5362e2367741bc5
Raaed Serag <raaedserag@gmail.com>
2023-05-03 21:32:25 +0300

    change allowed host

--- Blood_Bank/settings.py
+++ Blood_Bank/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "ec2-54-197-39-47.compute-1.amazonaws.com"
+]
 
 
 # Application definition
