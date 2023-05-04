900dca7d722633bc3cf2a937ac84216b96ce3ed3
Raaed Serag <raaedserag@gmail.com>
2023-05-04 19:16:05 +0300

    Update start-app.sh

--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-sleep 10
 
 cd /home/ubuntu/qatra-backend
 
-
-status_code=$(curl --write-out %{http_code} --silent --output /dev/null localhost:80)
-
-if [[ "$status_code" -ne 200 ]] ; then
-    echo "App is not running healthy on localhost:80"
-    exit 1
-else
-    echo "App is running healthy on localhost:80"
-    exit 0
-fi
+echo "starting server"
+sudo -E bash -c "source env/bin/activate; pm2 start server.json; pm2 save"
