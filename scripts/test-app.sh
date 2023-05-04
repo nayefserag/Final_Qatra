b9f712f08309838b01fac8e746b26258340be89e
Raaed Serag <raaedserag@gmail.com>
2023-05-04 19:16:18 +0300

    Update test-app.sh

--- scripts/test-app.sh
+++ scripts/test-app.sh
@@ -4,12 +4,12 @@ sleep 10
 cd /home/ubuntu/qatra-backend
 
 
-# status_code=$(curl --write-out %{http_code} --silent --output /dev/null localhost:8080)
+status_code=$(curl --write-out %{http_code} --silent --output /dev/null localhost:80)
 
-# if [[ "$status_code" -ne 200 ]] ; then
-#     echo "App is not running healthy on localhost:8080"
-#     exit 1
-# else
-#     echo "App is running healthy on localhost:8080"
-#     exit 0
-# fi
+if [[ "$status_code" -ne 200 ]] ; then
+    echo "App is not running healthy on localhost:80"
+    exit 1
+else
+    echo "App is running healthy on localhost:80"
+    exit 0
+fi
