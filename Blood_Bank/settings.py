9ff10b8da766a654d59d847cfa1238344f6a1d74
naooaf <nayfserag5@gmail.com>
2023-05-05 16:52:11 +0300

    restart

--- Blood_Bank/settings.py
+++ Blood_Bank/settings.py
@@ -95,28 +95,28 @@ WSGI_APPLICATION = 'Blood_Bank.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get("DB_USER"),
-        'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'HOST': os.environ.get("DB_HOST"),
-        'PORT': os.environ.get("DB_PORT"),
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'db',
-#         'USER': 'root',
-#         'PASSWORD': 'nayf',
-#         'HOST':'localhost', #127.0.0.1
-#         'PORT':'3306',
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get("DB_USER"),
+#         'PASSWORD': os.environ.get("DB_PASSWORD"),
+#         'HOST': os.environ.get("DB_HOST"),
+#         'PORT': os.environ.get("DB_PORT"),
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db',
+        'USER': 'root',
+        'PASSWORD': 'nayf',
+        'HOST':'localhost', #127.0.0.1
+        'PORT':'3306',
+    }
+}
 
 
 
