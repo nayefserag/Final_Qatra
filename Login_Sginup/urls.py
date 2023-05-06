b1096693e8b9e10671dff324cfb0b5811e2d4886
naooaf <nayfserag5@gmail.com>
2023-05-06 22:52:45 +0300

    Done

--- Login_Sginup/urls.py
+++ Login_Sginup/urls.py
@@ -34,7 +34,6 @@ urlpatterns = [
 
   #  path('password-reset/', views.PasswordResetView.as_view(), name='password_reset'),
   #  path('password-reset/done/', views.PasswordResetDoneView.as_view(template_name='Reset/password_reset_done.html'), name='password_reset_done'),
-   # path('password-reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
   #  path('password-reset-confirm/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
    # path('password-reset-complete/', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]   
